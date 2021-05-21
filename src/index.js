32acca185a0bfa2ca9f8927831ba5726348d7911
mikawebeng <84311892+mikawebeng@users.noreply.github.com>
2021-05-21 15:45:51 +0900

    TODO JS 完成

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import "./styles.css";
 const onClickadd = () => {
   const inputText = document.getElementById("add-txt").value;
   document.getElementById("add-txt").value = "";
+  createIncompleteList(inputText);
+};
 
+//未完了リスト関数化
+const createIncompleteList = (text) => {
   const div = document.createElement("div");
   div.className = "list-row";
 
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   document.getElementById("incomp-list").appendChild(div);
 
@@ -27,12 +31,18 @@ const onClickadd = () => {
     const returnButton = document.createElement("button");
     returnButton.innerText = "戻す";
 
+    returnButton.addEventListener("click", () => {
+      const deleteTarget2 = returnButton.parentNode;
+      document.getElementById("comp-list").removeChild(deleteTarget2);
+
+      const txt2 = deleteTarget2.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
     compTarget.appendChild(li);
     compTarget.appendChild(returnButton);
 
     document.getElementById("comp-list").appendChild(compTarget);
-
-    console.log(compTarget);
   });
 
   const deleteButton = document.createElement("button");
